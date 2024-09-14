16fb33142b31f2555540d52acf62cbd075ba40a2
vitor-caetano <vitor.e.caetano@gmail.com>
2024-09-14 02:19:10 -0300

    Remove intro

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -161,7 +161,6 @@ aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 aws_session_token = $AWS_SESSION_TOKEN
 aws_region = $AWS_REGION
-
 " >aws-creds.conf
 
     kubectl --namespace crossplane-system \
